1f426674f615b76aaf39135a26a2086626314f01
Kristóf Mátyás Marsi <mkmdev18@gmail.com>
2018-11-08 09:33:53 +0100

    Implemented second part of the DOJO

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, url_for, render_template, redirect
 
 app = Flask(__name__)
-counts = 0
+counts = {
+    'GET': 0,
+    'POST': 0,
+    'PUT': 0,
+    'DELETE': 0,
+    }
 
 
 @app.route('/')
@@ -9,12 +14,20 @@ def home():
     return render_template("index.html", counter=counts)
 
 
-@app.route('/request-counter', methods=['GET', 'POST'])
+@app.route('/request-counter', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def request_counter():
     global counts
+    # request_type = request.method
+
     if request.method == 'GET':
-        counts += 1
-        return redirect('/')
+        counts['GET'] += 1
+    elif request.method == 'POST':
+        counts['POST'] += 1
+    elif request.method == 'PUT':
+        counts['PUT'] += 1
+    elif request.method == 'DELETE':
+        counts['DELETE'] += 1
+    return redirect('/')
 
 
 if __name__ == '__main__':
